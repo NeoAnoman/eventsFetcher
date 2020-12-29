0238ee9fe48343392321be91ab6bcec67d958617
Neo Anoman <42651314+NeoAnoman@users.noreply.github.com>
2020-12-29 20:09:46 +0530

    scraper-developed

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const url = "https://dsc.community.dev/jaypee-institute-of-information-technology-sec-128/";
 var webdata =  "initial";
-var pastEvents = "";
+var pastEvents = [];
 var upcommingEvents = "";
 
 async function getData() {
@@ -12,9 +12,41 @@ async function getData() {
         webdata = res.data;
         console.log("I am loaded")
         const $ = cheerio.load(webdata);
-        var pastEvents = $('#past-events .vertical-boxes');
-        pastEvents = [].slice.call(pastEvents.getElementsByTagName('a'), 0);
+        if ($('#past-events .vertical-boxes a').attr('href') != undefined) {
+            $('#past-events .vertical-boxes a:nth-child(n)').each((i, elem)=> {
+                // console.log($(elem).find('p.vertical-box--event-date').text())
+                pastEvents.push({
+                    id: i+1,
+                    link: 'https://dsc.community.dev/' + $(elem).attr('href'),
+                    date: $(elem).find('p.vertical-box--event-date').text().replace(/  |\n/g, ''),
+                    type: $(elem).find('p.vertical-box--event-type').text().replace(/  |\n/g, ''),
+                    title: $(elem).find('p.vertical-box--event-title').text().replace(/  |\n/g, ''),
+                    image: $(elem).find('img').attr('src')
+                })
+            });
+        }
+        else {
+            pastEvents = $('#past-events .general-body--color').text();
+        }
+        if ($('#upcoming-events .vertical-boxes a').attr('href') != undefined) {
+            $('#upcoming-events .vertical-boxes a:nth-child(n)').each((i, elem) => {
+                // console.log($(elem).find('p.vertical-box--event-date').text())
+                pastEvents.push({
+                    id: i + 1,
+                    link: 'https://dsc.community.dev/' + $(elem).attr('href'),
+                    date: $(elem).find('p.vertical-box--event-date').text().replace(/  |\n/g, ''),
+                    type: $(elem).find('p.vertical-box--event-type').text().replace(/  |\n/g, ''),
+                    title: $(elem).find('p.vertical-box--event-title').text().replace(/  |\n/g, ''),
+                    image: $(elem).find('img').attr('src')
+                })
+            });
+        }
+        else {
+            upcommingEvents= $('#upcoming-events .general-body--color').text();
+        }
+        
         console.log(pastEvents);
+        console.log(upcommingEvents);
     })
         .catch(error => {
             console.log(error);
